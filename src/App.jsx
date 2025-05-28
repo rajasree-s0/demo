7517f6b5ded94410b648022b2a5dd854027a40ab
sree-s-08 <rajasrees08082003@gmail.com>
2025-05-28 10:19:23 +0530

    initial commit to branch b2

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <button>Branch B1</button>
+      <button>Branch B2</button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
